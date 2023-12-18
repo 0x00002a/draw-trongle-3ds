5e0cb6cfbdeb29bb6c5d3790eae9b4f59b0acea9
Natasha England-Elbro <natasha@natashaee.me>
2023-12-18 16:18:24 +0000

    feat: got it rendering

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,10 @@
 use citro3d::{
     attrib, buffer,
     macros::include_shader,
-    math::{FVec3, FVec4, IVec, Matrix, Matrix4},
+    math::{
+        AspectRatio, ClipPlanes, FVec3, FVec4, IVec, Matrix, Matrix4, Projection,
+        StereoDisplacement,
+    },
     render::{ClearFlags, Target},
     shader::{self, Program},
     texenv, Instance,
@@ -47,7 +50,7 @@ fn main() {
     let apt = Apt::new().unwrap();
     let mut hid = Hid::new().unwrap();
     let gfx = Gfx::new().unwrap();
-    //let _console = Console::new(gfx.bottom_screen.borrow_mut());
+    let _console = Console::new(gfx.bottom_screen.borrow_mut());
 
     let mut gpu = Instance::new().expect("failed to init citro3d");
     let mut gpu_buf = buffer::Info::new();
@@ -55,15 +58,15 @@ fn main() {
         [
             Vert {
                 pos: Vec3::new(0., 0.5, -3.),
-                colour: Vec3::new(1., 1., 1.),
+                colour: Vec3::new(0., 0., 1.),
             },
             Vert {
                 pos: Vec3::new(-0.5, -0.5, -3.),
-                colour: Vec3::new(1., 1., 1.),
+                colour: Vec3::new(1., 0., 0.),
             },
             Vert {
                 pos: Vec3::new(0.5, -0.5, -3.),
-                colour: Vec3::new(1., 1., 1.),
+                colour: Vec3::new(0., 1., 0.),
             },
         ],
         LinearAllocator,
@@ -92,13 +95,16 @@ fn main() {
         .src(texenv::Mode::BOTH, texenv::Source::PrimaryColor, None, None)
         .func(texenv::Mode::BOTH, texenv::CombineFunc::Replace);
 
+    let projections = calculate_projections();
+    println!("proj: {:#?}", projections.left_eye);
     while apt.main_loop() {
         gfx.wait_for_vblank();
 
         gpu.render_frame_with(|gpu| {
             target.clear(ClearFlags::ALL, 0, 0);
             gpu.select_render_target(&target).unwrap();
-            gpu.bind_vertex_uniform(proj_uniform, &Matrix4::identity());
+            let projections = calculate_projections();
+            gpu.bind_vertex_uniform(proj_uniform, &projections.left_eye);
             gpu.set_attr_info(&v_attrs);
             gpu.draw_arrays(buffer::Primitive::Triangles, buf_vtos);
         });
@@ -107,5 +113,42 @@ fn main() {
         if hid.keys_down().contains(KeyPad::START) {
             break;
         }
+    }
+}
+
+#[derive(Debug)]
+struct Projections {
+    left_eye: Matrix4,
+    right_eye: Matrix4,
+    center: Matrix4,
+}
+
+fn calculate_projections() -> Projections {
+    // TODO: it would be cool to allow playing around with these parameters on
+    // the fly with D-pad, etc.
+    let slider_val = ctru::os::current_3d_slider_state();
+    let interocular_distance = slider_val / 2.0;
+
+    let vertical_fov = 40.0_f32.to_radians();
+    let screen_depth = 2.0;
+
+    let clip_planes = ClipPlanes {
+        near: 0.01,
+        far: 100.0,
+    };
+
+    let (left, right) = StereoDisplacement::new(interocular_distance, screen_depth);
+
+    let (left_eye, right_eye) =
+        Projection::perspective(vertical_fov, AspectRatio::TopScreen, clip_planes)
+            .stereo_matrices(left, right);
+
+    let center =
+        Projection::perspective(vertical_fov, AspectRatio::BottomScreen, clip_planes).into();
+
+    Projections {
+        left_eye,
+        right_eye,
+        center,
     }
 }
